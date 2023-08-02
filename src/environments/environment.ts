68d29c6bd38e309b2a618eb8a6a3941808cebecd
Daly <dalyna64@gmail.com>
2023-08-02 16:29:13 +0100

    initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'oshop-6e813',
+    appId: '1:761570404578:web:5e5ea3393ea299fb2116bb',
+    storageBucket: 'oshop-6e813.appspot.com',
+    apiKey: 'AIzaSyCPHpFpkA9JzjSrdtGZ-ZlXn1cl6N6FzEU',
+    authDomain: 'oshop-6e813.firebaseapp.com',
+    messagingSenderId: '761570404578',
+    measurementId: 'G-CNLTT1B4W0',
+  },
+};
