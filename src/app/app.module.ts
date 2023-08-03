a94509e8c7649660eed9f1c50f6123863e2fd694
Daly <dalyna64@gmail.com>
2023-08-03 16:32:34 +0100

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
+import { AdminOrdersComponent } from './pages/admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './pages/admin/admin-products/admin-products.component';
+import { CheckOutComponent } from './pages/check-out/check-out.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MyOrdersComponent } from './pages/my-orders/my-orders.component';
+import { OrderSuccessComponent } from './pages/order-success/order-success.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
-import { CheckOutComponent } from './pages/check-out/check-out.component';
-import { OrderSuccessComponent } from './pages/order-success/order-success.component';
-import { MyOrdersComponent } from './pages/my-orders/my-orders.component';
-import { AdminProductsComponent } from './pages/admin/admin-products/admin-products.component';
-import { AdminOrdersComponent } from './pages/admin/admin-orders/admin-orders.component';
-import { LoginComponent } from './pages/login/login.component';
 
-let Material = [MatToolbarModule, MatIconModule];
+let Material = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatMenuModule,
+];
 @NgModule({
   declarations: [
     AppComponent,
