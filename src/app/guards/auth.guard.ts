39b51a4b632a95d4393bb65b1a38813856b6364f
Daly <dalyna64@gmail.com>
2023-08-09 14:04:58 +0100

    finalisation de l'auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,7 +1,14 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
   let isLoged = localStorage.getItem('isLoged');
+  let router = inject(AuthService).router;
+  console.log('url : ', route.url);
+
   if (isLoged == 'true') return true;
+
+  router.navigate(['login']);
   return false;
 };
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { ActivatedRoute } from '@angular/router';
 import firebase from 'firebase/compat/app';
 import { Observable } from 'rxjs';
 @Injectable({
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 export class AuthService {
   $user: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth, private router: ActivatedRoute) {
+  constructor(private afAuth: AngularFireAuth, public router: Router) {
     if (afAuth.authState) {
       this.$user = afAuth.authState as Observable<firebase.User>;
     }
@@ -19,9 +19,11 @@ export class AuthService {
     localStorage.setItem('isLoged', 'true');
 
     this.afAuth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    this.router.navigate(['check-out']);
   }
   logout() {
     localStorage.setItem('isLoged', 'false');
     this.afAuth.signOut();
+    this.router.navigate(['login']);
   }
 }
